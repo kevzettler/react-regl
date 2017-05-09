d6ef7e4901bb80606b469ad91fe7605a0b610f1e
kevzettler <kevzettler@gmail.com>
2017-05-09 14:32:49 -0700

    Actually run build

--- dist/util/topDownDrawScopes.js
+++ dist/util/topDownDrawScopes.js
@@ -3,6 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _batchChildren = require("./batchChildren.js");
+
+var _batchChildren2 = _interopRequireDefault(_batchChildren);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var topDownDrawScopes = function topDownDrawScopes(node) {
   if (!node) {
     return function () {};
@@ -15,7 +22,7 @@ var topDownDrawScopes = function topDownDrawScopes(node) {
   }
 
   if (node.children) {
-    var children = batchChildren(node.children);
+    var children = (0, _batchChildren2.default)(node.children);
 
     if (node.data.drawCommand) {
       return function () {
