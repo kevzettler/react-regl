fc3470b875e39200b3fe881465d56f9dda0fadd5
kevzettler <kevzettler@gmail.com>
2017-05-08 21:58:57 -0700

    test child draw functions

--- test/util/topDownDrawScopes-test.js
+++ test/util/topDownDrawScopes-test.js
@@ -1,7 +1,27 @@
 import test from 'tape';
 import topDownDrawScopes from '../../src/util/topDownDrawScopes.js';
+import Node from 'display-tree';
+import sinon from 'sinon';
 
 test('topDownDrawScopes', (t) => {
   t.equal(typeof topDownDrawScopes(), 'function', "it returns a function");
+
+  
+  t.test("a node with no children", (st) => {
+    const drawFn = sinon.spy();
+    const props = {
+      cool: 'bro',
+      drawCommand: drawFn
+    };
+
+    const tNode = Node(props);
+
+    topDownDrawScopes(tNode)();
+
+    st.equal(drawFn.callCount, 1, "should call its draw function");
+    st.end();
+  });
+  
+
   t.end();
 });
