90b06b45f82584576b72f54ed40a1789eccba0b8
kevzettler <kevzettler@gmail.com>
2017-11-27 15:15:15 -0600

    WIP new tests

--- __tests__/components/Regl.test.js
+++ __tests__/components/Regl.test.js
@@ -1,3 +1,6 @@
-it('renders', () => {
-  expect(true).toBeFalsy();
-});
+import React from 'react';
+import Regl from '../../src/index.js';
+
+it('should render a canvas', () => {
+  expect(render(<Regl width={500} height={400} />)).toMatchSnapshot();
+})
