89367dd98f8498790738e1809e86a3a17d0586a7
kevzettler <kevzettler@gmail.com>
2017-11-28 11:34:51 -0600

    WIP storybook setup

--- stories/index.js
+++ stories/index.js
@@ -5,7 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { withInfo } from '@storybook/addon-info';
 
 import Regl, { Draw } from '../src/';
 
@@ -34,42 +34,44 @@ storiesOf('Regl', module)
   });
 
 
-storiesOf('Draw/2D', module)
-  .addDecorator(withKnobs)
-  .add('static', () => {
-    return (
-      <Regl
-          width={window.innerWidth}
-          height={window.innerHeight}
-          color={[0,0,0,1]}
-      >
-      <Draw
-        vert={`
+storiesOf('Draw', module)
+  .add('simple info',
+       withInfo({
+         text: 'String or React Element with docs about my component', // Warning! This option's name will be likely renamed to "summary" in 3.3 release. Follow this PR #1501 for details
+         // other possible options see in Global options section below
+       })(() =>
+         <Regl
+           width={window.innerWidth}
+           height={window.innerHeight}
+           color={[0,0,0,1]}
+         >
+           <Draw
+             vert={`
           precision mediump float;
           attribute vec2 position;
           void main () {
             gl_Position = vec4(position, 0, 1);
           }`}
 
-        frag={`
+             frag={`
           precision mediump float;
           uniform vec4 color;
           void main () {
             gl_FragColor = color;
           }`}
 
-        attributes={{
-          position: [
-            [-1, 0],
-            [0, -1],
-            [1, 1]
-          ]
-        }}
-        uniforms={{
-          color: [1, 0, 0, 1]
-        }}
-        count={3}
-      />
-      </Regl>
-    );
-  });
+             attributes={{
+               position: [
+                 [-1, 0],
+                 [0, -1],
+                 [1, 1]
+               ]
+             }}
+             uniforms={{
+               color: [1, 0, 0, 1]
+             }}
+             count={3}
+           />
+         </Regl>
+       )
+  )
