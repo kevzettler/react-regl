c25687cd9d42a93b5e175e6d3a52e523ff8b0f98
kevzettler <kevzettler@gmail.com>
2018-02-22 13:31:42 -0800

    successfully extracted draw definition iterators

--- src/nodes/DrawNode.js
+++ src/nodes/DrawNode.js
@@ -1,6 +1,17 @@
 import Node from '../nodes/Node.js';
 
-function definitionPropReducer(props, regl, definitionKey, acc, reglProp){
+const reglWhitelist = [
+  'vert', 'frag', 'primitive',
+  'offset', 'instances', 'elements', 'profile', 'depth',
+  'blend', 'stencil', 'cull', 'polygonOffset', 'scissor'
+];
+
+function uniformArrayUnrollReducer(regl, definitionKey, reglProp, accum, value, index){
+  accum[`${reglProp}[${index}]`] = regl.prop(`${definitionKey}.${reglProp}[${index}]`)
+  return accum;
+}
+
+function subLevelPropReducer(props, regl, definitionKey, acc, reglProp){
   //Need to unroll array uniforms
   // https://github.com/regl-project/regl/issues/258
   // https://github.com/regl-project/regl/issues/373
@@ -8,13 +19,7 @@ function definitionPropReducer(props, regl, definitionKey, acc, reglProp){
      Array.isArray(props[definitionKey][reglProp]) &&
      props[definitionKey][reglProp].filter((ele) => { return Array.isArray(ele)}).length > 0
   ){
-    const unrolled = [...Array(props[definitionKey][reglProp].length)].reduce((accum, value, index) => {
-      accum[`${reglProp}[${index}]`] = regl.prop(`${definitionKey}.${reglProp}[${index}]`)
-      return accum;
-    }, {});
-
-    //return acc[reglProp] = unrolled;
-    //not sure what this was doing cloning props?
+    const unrolled = [...Array(props[definitionKey][reglProp].length)].reduce(uniformArrayUnrollReducer.bind(this, regl, definitionKey, reglProp), {});
     return {...acc, ...unrolled};
   }
 
@@ -28,38 +33,35 @@ function definitionPropReducer(props, regl, definitionKey, acc, reglProp){
   return acc;
 }
 
-const getReglDrawDefinitionFromProps = (props, regl) =>{
-  const reglDefinition = {};
+//Iterate over top level react component keys like attributes, uniforms, count, elements
+function topLevelKeyReducer(reactProps, regl, reglDefinition, topLevelDefinitionKey){
+  if(reglWhitelist.indexOf(topLevelDefinitionKey) !== -1){
+    reglDefinition[topLevelDefinitionKey] = reactProps[topLevelDefinitionKey];
+    return reglDefinition;
+  }
 
-  const reglWhitelist = [
-    'vert', 'frag', 'primitive',
-    'offset', 'instances', 'elements', 'profile', 'depth',
-    'blend', 'stencil', 'cull', 'polygonOffset', 'scissor'
-  ];
+  //iterate over sub level keys for position and uniforms
+  if(['attributes', 'uniforms'].indexOf(topLevelDefinitionKey) !== -1){
+    reglDefinition[topLevelDefinitionKey] = Object.keys(reactProps[topLevelDefinitionKey])
+                                          .reduce(subLevelPropReducer.bind(this, reactProps, regl, topLevelDefinitionKey), {});
+    return reglDefinition;
+  }
 
-  Object.keys(props).forEach((definitionKey) => {
-    if(reglWhitelist.indexOf(definitionKey) !== -1){
-      reglDefinition[definitionKey] = props[definitionKey];
-      return;
-    }
+  if(['string', 'number', 'boolean'].indexOf(typeof reactProps[topLevelDefinitionKey]) !== -1){
+    reglDefinition[topLevelDefinitionKey] = regl.prop(topLevelDefinitionKey);
+    return reglDefinition;
+  }
 
-    //iterate over sub level keys for position and uniforms
-    if(['attributes', 'uniforms'].indexOf(definitionKey) !== -1){
-      reglDefinition[definitionKey] = Object.keys(props[definitionKey])
-                                            .reduce(definitionPropReducer.bind(this, props, regl, definitionKey), {});
-    }
-
-    if(['string', 'number', 'boolean'].indexOf(typeof props[definitionKey]) !== -1){
-      reglDefinition[definitionKey] = regl.prop(definitionKey);
-      return;
-    }
-
-    if(reglWhitelist.indexOf(definitionKey) !== -1){
-      reglDefinition[definitionKey] = regl.prop(definitionKey);
-    }
-  });
+  if(reglWhitelist.indexOf(topLevelDefinitionKey) !== -1){
+    reglDefinition[topLevelDefinitionKey] = regl.prop(topLevelDefinitionKey);
+    return reglDefinition;
+  }
 
   return reglDefinition;
+};
+
+const getReglDrawDefinitionFromProps = (reactProps, regl) =>{
+  return Object.keys(reactProps).reduce(topLevelKeyReducer.bind(this, reactProps, regl), {});
 }
 
 export default class DrawNode extends Node {
