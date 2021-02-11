25593e36ede4baddaf9893440d7605c79bcf5581
Kev Zettler <kevzettler@gmail.com>
2021-02-11 14:00:03 -0800

    Type cleanup and experiments

--- src/reactRegl.ts
+++ src/reactRegl.ts
@@ -1,18 +1,20 @@
 /// <reference path="./types/deferred-regl.d.ts" />
 /// <reference types="./types/deferred-regl" />
-import React from 'react';
+import React, { ReactChildren } from 'react';
 import PropTypes from 'prop-types'
+import Regl from 'regl'
 import defregl, { IDregl } from 'deferred-regl';
-import { DrawConfig, DrawCommand, DefaultContext, DynamicVariable } from 'regl'
+
 
 const dregl: IDregl = defregl();
 
-export interface ReactReglComponent<DefinitionProps> extends DrawCommand {
+
+export interface ReactReglComponent<DefinitionProps> {
   <
    ExecutionProps extends {} = {}
   >(
     executionProps: ExecutionProps, contextOrRef: {reactify?: boolean}
-  ): React.ReactElement<DefinitionProps & ExecutionProps & IDregl, 'ReglDraw'>,
+  ): React.ReactElement<DefinitionProps & ExecutionProps & IDregl, 'ReglDraw'> | void
 }
 
 interface ReactRegl extends IDregl{
@@ -21,27 +23,32 @@ interface ReactRegl extends IDregl{
   Attributes extends {} = {},
   Props extends {} = {},
   OwnContext extends {} = {},
-  ParentContext extends DefaultContext = DefaultContext
+  ParentContext extends Regl.DefaultContext = Regl.DefaultContext
   >(
-    drawConfig: DrawConfig<Uniforms, Attributes, Props, OwnContext, ParentContext>,
+    drawConfig: Regl.DrawConfig<Uniforms, Attributes, Props, OwnContext, ParentContext>,
   ): ReactReglComponent<Props>
 
   //FIXME
   // this is an override for the regl.prop method
   // theres some magic with the base regl.prop declaration that is failing
   // https://github.com/regl-project/regl/issues/602
-  prop<Key>(name: Key): DynamicVariable<Key>;
+  prop<Key>(name: Key): Regl.DynamicVariable<Key>;
 }
 
 // Wrap the deferred-regl wrapper
 // so that it returns a react element when called
 // within a react context
-const reactRegl: unknown = function(definitionProps: DrawConfig){
-  let drawCommand: DrawCommand | null = null;
-  const ReactReglComponent = (executionProps: any, contextOrRef: any) => {
+const reactRegl: unknown = function(definitionProps: Regl.DrawConfig){
+  let drawCommand: Regl.DrawCommand | null = null;
+  // TODO execution props should enforce type safety of DynamicVariables for user defined `regl.prop`
+  const ReactReglComponent = (
+    executionProps: Partial<typeof definitionProps & {children?: ReactChildren}>,
+    contextOrRef: any
+  ) => {
     if(contextOrRef?.reactify === true){
       const merged = {definitionProps, executionProps, dregl};
-      return React.createElement('ReglDraw', merged, executionProps.children);
+      const children = executionProps.children ? executionProps.children : null
+      return React.createElement('ReglDraw', merged, children);
     }
 
     if(drawCommand === null){
@@ -57,9 +64,10 @@ const reactRegl: unknown = function(definitionProps: DrawConfig){
   ReactReglComponent.contextTypes = {
     reactify: PropTypes.bool
   }
+
   return ReactReglComponent
 }
-// set prototype with extended regl resource generators
-Object.setPrototypeOf(reactRegl, dregl);
+Object.setPrototypeOf(reactRegl, dregl)
 
-export default reactRegl as ReactRegl
+
+export default reactRegl as ReactRegl;
