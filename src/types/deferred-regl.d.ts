7df89021103e1cf2b0c470010e4ef206c8050580
Kev Zettler <kevzettler@gmail.com>
2021-06-29 08:54:24 -0700

    update to latest deferred-regl

--- src/types/deferred-regl.d.ts
+++ src/types/deferred-regl.d.ts
@@ -1,13 +1,17 @@
 declare module "deferred-regl"{
-  import { Regl, DrawConfig } from 'regl'
+  import { Regl, DrawConfig, Limits } from 'regl'
 
   type IDregl<T> = {
     setRegl: (regl?: Regl) => void
     queue: any[]
     setQueue: (queuInput: any[]) => void
+    map: {[key: string]: () => any}
+    replicateTo: (target: IDregl<Regl>) => void
     (drawConfig? : DrawConfig): IDregl<Regl>
   } & {
     [K in keyof T]: () => T[K]
+  } & {
+    limits: Limits
   };
 
   export type DeferredRegl = IDregl<Regl>
